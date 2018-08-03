3c20de27b0fb4bbf63cb97782d228075bf067118
Suele <suelen.ads.guimaraes@gmail.com>
2018-08-03 09:23:36 -0300

    Foi adicionado ao projeto um arquivo que faz o controle de uma negociação.

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,17 +1,29 @@
-class NegociacaoController{
-
-    constructor(){
+class NegociacaoController {
+    
+    constructor() {
+        
         let $ = document.querySelector.bind(document);
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
     }
-
-    adiciona(event){
-        event.preventDefault();
+    
+    adiciona(event) {
         
-        console.log(this._inputData.value);
-        console.log(this._inputQuantidade.value);
-        console.log(this._inputValor.value);
+        event.preventDefault();
+      
+        let data = new Date(
+            ...this._inputData.value
+            .split('-')
+            .map((item, indice) => item - indice % 2)
+        );
+            
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value  
+        );
+        
+        console.log(negociacao);
     }
 }
