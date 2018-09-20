4d2f0d9bb721f43bd43c5094d0abce53eb1272f4
Suele <suelen.ads.guimaraes@gmail.com>
2018-09-19 23:49:27 -0300

    add

--- aluraframe/client/js/app/helpers/DateHelper.js
+++ aluraframe/client/js/app/helpers/DateHelper.js
@@ -1,22 +1,20 @@
 class DateHelper {
     
-  constructor() {
+    constructor() {
         
-    throw new Error('Esta classe não pode ser instanciada');
-  }
-    
-  static dataParaTexto(data) {
-    
-    return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
-  }
-    
-  static textoParaData(texto) {
-        
-    if(!/\d{4}-\d{2}-\d{2}/.test(texto)) {
-      throw new Error('Deve estar no formato aaaa-mm-dd');
+        throw new Error('Esta classe não pode ser instanciada');
     }
+    
+    static dataParaTexto(data) {
+        return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
+    }
+    
+    static textoParaData(texto) {
+        
+        if(!/\d{4}-\d{2}-\d{2}/.test(texto)) 
+            throw new Error('Deve estar no formato aaaa-mm-dd');
              
-    return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
-  }
+        return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
+    }
     
 }
--- aluraframe/client/js/app/views/NegociacoesView.js
+++ aluraframe/client/js/app/views/NegociacoesView.js
@@ -1,47 +1,43 @@
-class NegociacoesView{
-
-    constructor(elemento){
-      this._elemento = elemento;
-    }
-
-  _template(listaDeNegociacoes){
-    return `
-      <table class="table table-hover table-bordered">
-        <thead>
-          <tr>
-            <th>DATA</th>
-            <th>QUANTIDADE</th>
-            <th>VALOR</th>
-            <th>VOLUME</th>
-          </tr>
-        </thead>
-              
-        <tbody>
-          ${listaDeNegociacoes.negociacoes.map((negociacao) => {
-            return `
-              <tr>
-                <td>${DateHelper.dataParaTexto(negociacao.data)}</td>
-                <td>${negociacao.quantidade}</td>
-                <td>${negociacao.valor}</td>
-                <td>${negociacao.volume}</td>
-              </tr>
-            `
-          }).join('')}
-        </tbody>
-              
-        <tfoot>
-          <td colspan="3"></td>
-          <td>${listaDeNegociacoes.negociacoes.reduce(function(total, negociacao) {
-            return total + negociacao.volume;
-            }, 0.0)}
-          </td>
-        </tfoot>
-      </table>
-   `;
-  }
-
-  atualiza(listaDeNegociacoes){
+class NegociacoesView extends View {
     
-    this._elemento.innerHTML = this._template(listaDeNegociacoes);
-  }
+    constructor(elemento) {
+        super(elemento);
+    }
+    
+    template(model) {
+        
+        return `
+        <table class="table table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th>DATA</th>
+                    <th>QUANTIDADE</th>
+                    <th>VALOR</th>
+                    <th>VOLUME</th>
+                </tr>
+            </thead>
+        
+            <tbody>
+                ${model.negociacoes.map(n => `
+                    
+                    <tr>
+                        <td>${DateHelper.dataParaTexto(n.data)}</td>
+                        <td>${n.quantidade}</td>
+                        <td>${n.valor}</td>
+                        <td>${n.volume}</td>
+                    </tr>
+                    
+                `).join('')}                
+            </tbody>
+                  
+            <tfoot>
+                <td colspan="3"></td>
+                <td>
+                    ${model.negociacoes.reduce((total, n) => total + n.volume, 0.0)}
+                </td>
+            </tfoot>
+            
+        </table>
+        `;
+    }
 }
